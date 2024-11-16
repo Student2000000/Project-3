76cee140650eca212ef9a8229d92fff6ec61db81
Jay A <Jenna.Arends@Seattlecolleges.edu>
2024-11-16 15:11:18 -0800

    Entered Main and Parser Classes

---
+++ src/main/java/Main.java
@@ -0,0 +1,3 @@
+public class Main {
+    //Run methods here- keep it breif don't overdo it
+}
---
+++ src/main/java/Parser.java
@@ -0,0 +1,18 @@
+public class Parser {
+    /*
+    Methods for this class from the instructions:
+    1) Varius constructos
+        the binary serach trees and lists I presume
+    2) intializeReservedWords
+        Read in the file of reserved words and put them into your
+        reserved words data structure. Then use setBalancedBST
+        to set up a balanced BST from them.
+    3) SetBalancedBST
+        Add a reserved word and ensure binary search tree is balanced.
+    4) getIdentifiers
+        Finds the identifiers in a Java program and adds them to the
+        identifiers BST.
+     */
+
+
+}
